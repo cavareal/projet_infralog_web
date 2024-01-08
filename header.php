cbd07678b4bb413a7c4366b45a67b5152a0d4784
CarreauTh <tho.carreau@gmail.com>
2024-01-08 11:18:56 +0100

    Modification du header

--- header.php
+++ header.php
@@ -2,10 +2,11 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="./favicon.png">
+    <link rel="icon" href="../favicon.png">
 
-    <link href="./css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 
     <!-- Latest bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -15,15 +16,13 @@
 </head>
 
 <body>
-    <div class="shadow">
-        <nav class="justify-content-end"
-    
+    <div style="background-color: white; display: flex; align-items: center;" class="shadow sticky-top">
 
-        <nav style="background-color: white;" class="navbar navbar-expand-md sticky-top">
+        <nav style="width: 90%;" class="navbar navbar-expand-md sticky-top">
             <div class="container-fluid">
                 <a class="navbar-brand" href="./">
-                    <img src="favicon.png" alt="Logo-favicon.png" style="width: 40px;" class="rounded-pill">
-                    <img src="logo_nom.png" alt="Logo-favicon.png" style="height: 40px;" >
+                    <img src="../favicon.png" alt="Logo-favicon.png" style="width: 40px;" class="rounded-pill">
+                    <img src="../logo_nom.png" alt="Logo-favicon.png" style="height: 40px;" >
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
@@ -45,6 +44,12 @@
                 </div>
             </div>
         </nav>
+
+        <div style="float: right; width: 10%; display: flex; padding: 12px; justify-content: end;">
+            <a class="" href="./">
+                <img src="../default_picture.png" alt="Profile_picture" class="rounded-pill" style="width: 30px;">
+            </a>
+        </div>
     </div>
 
 </body>
