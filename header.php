8583cda4a16c5b306ed95dedadf8e9240f5ba4d5
Alexisse <alexis.bajolle@reseau.eseo.fr>
2024-01-15 19:16:09 +0100

    rectification header logo

--- header.php
+++ header.php
@@ -6,11 +6,6 @@
 
     <link rel="icon" href="./favicon.png">
 
-<<<<<<< Updated upstream
-    
-
-=======
->>>>>>> Stashed changes
     <!-- Latest bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -18,12 +13,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <link href="./css/style.css" rel="stylesheet">
-<<<<<<< Updated upstream
-</head>
-
-<body>
-  
-=======
 
 </head>
 
@@ -36,43 +25,18 @@
     }
     ?>
 
->>>>>>> Stashed changes
     <header>
         <nav class="navbar navbar-expand-md bg-white sticky-top shadow-lg">
             <div class="container-fluid">
                 <a class="navbar-brand" href="./">
-<<<<<<< Updated upstream
-                    <img src="./images/favicon.png" alt="Logo-favicon.png" class="rounded-pill">
-                    <img src="./images/logo_nom.png" alt="Logo-favicon.png">
-=======
+
                     <img src="./favicon.png" alt="Logo-favicon.png" class="rounded-pill">
                     <img src="./logo_nom.png" alt="Logo-favicon.png">
->>>>>>> Stashed changes
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-<<<<<<< Updated upstream
-                
-                <div class="collapse navbar-collapse" id="mynavbar">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
-                
-                    <div class="d-flex">
-                        <a href="./connection.php?form=connexion" class="btn border">Se connecter</a>
-                        <a href="./connection.php" class="btn border">Créer mon compte</a>
-                    </div>
-=======
 
                 <div class="collapse navbar-collapse" id="mynavbar">
                     <ul class="navbar-nav me-auto">
@@ -108,7 +72,7 @@
                             <img src="./default_picture.png" alt="default_picture.png" class="rounded-pill">
                         </div>
                     <?php } ?>
->>>>>>> Stashed changes
+
                 </div>
             </div>
         </nav>
