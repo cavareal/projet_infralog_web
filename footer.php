70271994cd22bb0929efc68a90d0caa9a4cef311
CarreauTh <tho.carreau@gmail.com>
2024-01-18 17:12:11 +0100

    Suppression de fichiers redondants

--- footer.php
+++ footer.php
@@ -3,18 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-     <link rel="icon" href="./favicon.png">
-
-     <link href="./css/style.css" rel="stylesheet">
-
-     <!-- Latest bootstrap 5 CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-     <!-- Latest bootstrap 5 JavaScript -->
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </head>
+
 <body>
     <footer class="container-flex shadow">
         <nav class="navbar navbar-expand-md sticky-top">
